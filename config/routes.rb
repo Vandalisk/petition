b855a0b19c4bd67c961ecbc1866f6c0bd4c2d596
ILya <zakharuni13@gmail.com>
2015-07-31 01:19:44 +0300

    some changies in rout

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'petition_entities/:id/subscribe_petition' => 'petition_entities#subscribe_petition', as: :subscribe_petition
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :petition_entities
+  resources :petition_entities
 
   # Example resource route with options:
   #   resources :products do
