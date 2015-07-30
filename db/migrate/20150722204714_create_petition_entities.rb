2807127a2e064d6461106103d9ed5e4b1363d483
ILya <zakharuni13@gmail.com>
2015-07-31 01:14:54 +0300

    added some corrections and additions to models

--- db/migrate/20150722204714_create_petition_entities.rb
+++ db/migrate/20150722204714_create_petition_entities.rb
@@ -3,7 +3,7 @@ class CreatePetitionEntities < ActiveRecord::Migration
     create_table :petition_entities do |t|
       t.string :title
       t.string :description
-      t.integer :votes
+      t.text :votes, array: true, default: []
       t.timestamps null: false
     end
   end
